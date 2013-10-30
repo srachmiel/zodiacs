07b526d8ed6765710643cc125f5a904719045a3a
Sarah Rachmiel <srachmiel2014@kellogg.northwestern.edu>
2013-10-30 08:42:53 -0500

    Homework 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 FortuneTeller::Application.routes.draw do
   get("/lucky_numbers", { :controller => "fortunes", :action => "numbers" })
 
-  
+  get("/zodiacs/:sign", { :controller => "zodiacs", :action => "show" })
+
   #get("/zodiacs/aries",       { :controller => "zodiacs", :action => "ram" })
   #get("/zodiacs/taurus",      { :controller => "zodiacs", :action => "bull" })
   #get("/zodiacs/gemini",      { :controller => "zodiacs", :action => "twins" })
